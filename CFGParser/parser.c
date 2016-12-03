afaa9c8cc3dd1fd3c07154c5ee8d5bd2a964cc62
Ergun Batuhan Öktem <batuhanoktem@gmail.com>
2016-12-04 00:03:13 +0300

    readWords function has been finished, so results output file can be generated

--- CFGParser/parser.c
+++ CFGParser/parser.c
@@ -2,35 +2,47 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define TRUE 1
+#define FALSE 0
+
 typedef struct Rule
 {
 	int id;
 	char left;
 
 	int rightCount;
-	char* right;
+	char *right;
 } Rule;
 
 typedef struct Parser
 {
 	int terminalCount;
-	char* terminals;
-	
+	char *terminals;
+
 	int nonTerminalCount;
-	char* nonTerminals;
+	char *nonTerminals;
 
 	int ruleCount;
-	Rule* rules;
+	Rule *rules;
 } Parser;
 
 Parser parser;
-void readGrammar(char* grammar);
-void readWords(char* words);
+void readGrammar(char *grammar);
+void readWords(char *words, char *results);
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
-	readGrammar(argv[1]);
-	readWords(argv[2]);
+	if (argc == 4)
+	{
+		readGrammar(argv[1]);
+		readWords(argv[2], argv[3]);
+	}
+	else
+	{
+		readGrammar("grammar.txt");
+		readWords("words.txt", "results.txt");
+	}
+
 	return 0;
 }
 
@@ -51,9 +63,9 @@ void readGrammar(char* grammar)
 			{
 				parser.terminalCount++;
 				if (parser.terminalCount == 1)
-					parser.terminals = (char*)malloc(sizeof(char));
+					parser.terminals = (char*)malloc(2 * sizeof(char));
 				else
-					parser.terminals = realloc(parser.terminals, parser.terminalCount * sizeof(char));
+					parser.terminals = realloc(parser.terminals, (parser.terminalCount + 1) * sizeof(char));
 
 				parser.terminals[parser.terminalCount - 1] = character;
 			}
@@ -73,9 +85,9 @@ void readGrammar(char* grammar)
 			{
 				parser.nonTerminalCount++;
 				if (parser.nonTerminalCount == 1)
-					parser.nonTerminals = (char*)malloc(sizeof(char));
+					parser.nonTerminals = (char*)malloc(2 * sizeof(char));
 				else
-					parser.nonTerminals = realloc(parser.nonTerminals, parser.nonTerminalCount * sizeof(char));
+					parser.nonTerminals = realloc(parser.nonTerminals, (parser.nonTerminalCount + 1) * sizeof(char));
 
 				parser.nonTerminals[parser.nonTerminalCount - 1] = character;
 			}
@@ -88,10 +100,12 @@ void readGrammar(char* grammar)
 	{
 		Rule rule;
 
-		fscanf(file, "%d    %c", &rule.id, &rule.left);
+		fscanf(file, "%d %c", &rule.id, &rule.left);
 
 		character = fgetc(file);
-		if ((character != ' ') || (character == '\n') || (character != '\r'))
+		if (feof(file))
+			break;
+		else if ((character != ' ') || (character == '\n') || (character != '\r'))
 		{
 			while ((character = fgetc(file)) != '>')
 			{
@@ -99,7 +113,7 @@ void readGrammar(char* grammar)
 			}
 
 			rule.rightCount = 0;
-			while (1)
+			while (TRUE)
 			{
 				character = fgetc(file);
 				if (feof(file))
@@ -112,9 +126,9 @@ void readGrammar(char* grammar)
 					{
 						rule.rightCount++;
 						if (rule.rightCount == 1)
-							rule.right = (char*)malloc(sizeof(char));
+							rule.right = (char*)malloc(2 * sizeof(char));
 						else
-							rule.right = realloc(rule.right, rule.rightCount * sizeof(char));
+							rule.right = realloc(rule.right, (rule.rightCount + 1) * sizeof(char));
 
 						rule.right[rule.rightCount - 1] = character;
 					}
@@ -134,24 +148,134 @@ void readGrammar(char* grammar)
 	fclose(file);
 }
 
-void readWords(char* words)
+void readWords(char *words, char *results)
 {
+	int i, j;
 	char character;
 	int wordCount = 0;
 	char *word = NULL;
 	FILE *file = fopen(words, "r");
+	FILE *outputFile = fopen(results, "w");
 
 	while (!feof(file))
 	{
 		character = fgetc(file);
-		if (feof(file))
-			break;
 
-		else if ((character != ' '))
+		if ((character != ' '))
 		{
-			if (character == '\n')
+			if (character == '\n' || feof(file))
 			{
+				if (wordCount == 0)
+					break;
+
+				int illegalSymbol;
 				word[wordCount] = '\0';
+
+				for (i = 0; i < wordCount - 1; i++)
+				{
+					illegalSymbol = TRUE;
+					for (j = 0; j < parser.nonTerminalCount; j++)
+					{
+						if (word[i] == parser.nonTerminals[j])
+						{
+							illegalSymbol = FALSE;
+							break;
+						}
+					}
+
+					if (illegalSymbol)
+					{
+						for (j = 0; j < parser.terminalCount; j++)
+						{
+							if (word[i] == parser.terminals[j])
+							{
+								illegalSymbol = FALSE;
+								break;
+							}
+						}
+					}
+
+					if (illegalSymbol)
+						break;
+				}
+
+				if (illegalSymbol)
+					fprintf(outputFile, "NO\n");
+				else
+				{
+					int accepted = FALSE;
+					int *acceptedRules = NULL;
+					int startIndex;
+					int endIndex;
+					int acceptedRule = 0;
+					char *lastWord = (char*)malloc((wordCount + 1) * sizeof(char));
+					char *tempWord;
+					strcpy(lastWord, word);
+					for (endIndex = wordCount - 1; endIndex >= 0; endIndex--)
+					{
+						for (startIndex = endIndex; startIndex >= 0; startIndex--)
+						{
+							int length = endIndex - startIndex + 1;
+							char *wordPart = (char*)malloc((length + 1) * sizeof(char));
+							strncpy(wordPart, lastWord + startIndex, length);
+							wordPart[length] = '\0';
+							for (j = 0; j < parser.ruleCount; j++)
+							{
+								Rule rule = parser.rules[j];
+
+								if (strcmp(wordPart, rule.right) == 0)
+								{
+									acceptedRule++;
+									if (acceptedRule == 1)
+										acceptedRules = (int*)malloc(sizeof(int));
+									else
+										acceptedRules = realloc(acceptedRules, acceptedRule * sizeof(int));
+
+									acceptedRules[acceptedRule - 1] = rule.id;
+
+									tempWord = (char*)malloc((wordCount + 1) * sizeof(char));
+									strncpy(tempWord, lastWord, startIndex);
+									tempWord[startIndex] = rule.left;
+									tempWord[startIndex + 1] = '\0';
+									strcat(tempWord, lastWord + endIndex + 1);
+
+									lastWord = tempWord;
+									wordCount -= length - 1;
+									endIndex = wordCount - 1;
+									startIndex = endIndex + 1;
+
+									if ((wordCount == 1) && (lastWord[0] == parser.nonTerminals[0]))
+										accepted = TRUE;
+
+									break;
+								}
+
+								if (accepted)
+									break;
+							}
+
+							if (accepted)
+								break;
+						}
+
+						if (accepted)
+							break;
+					}
+					if (!accepted)
+						fprintf(outputFile, "NO\n");
+					else
+					{
+						fprintf(outputFile, "YES\n");
+						for (j = acceptedRule - 1; j >= 0; j--)
+						{
+							fprintf(outputFile, "%d", acceptedRules[j]);
+							if (j != 0)
+								fprintf(outputFile, " ");
+						}
+						fprintf(outputFile, "\n");
+					}
+				}
+
 				wordCount = 0;
 			}
 			else if (character != '\r')
@@ -167,4 +291,5 @@ void readWords(char* words)
 		}
 	}
 	fclose(file);
+	fclose(outputFile);
 }
